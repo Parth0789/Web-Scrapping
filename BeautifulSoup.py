190563c3b0c911cc26bee7ee0b010dd57af2d66b
Parth0789 <parthmanchanda0789@gmail.com>
2018-11-27 17:32:57 +0530

    web scrapping
    web scrapping using python.
    Library used:
    Pandas
    bs4
    requests
    numpy

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,7 +1,11 @@
 import requests
 import numpy as np
 from bs4 import BeautifulSoup
+import pandas as pd
 
+place = []
+username = []
+xp =[]
 np.set_printoptions(sign = ' ')
 #get the data
 data = requests.get('https://umggaming.com/leaderboards')
@@ -13,7 +17,10 @@ leaderboad = soup.find('table', {'id': 'leaderboard-table'})
 tbody = leaderboad.find('tbody')
 
 for tr in tbody.find_all('tr'):
-    place = tr.find_all('td')[0].text.strip()
-    username = tr.find_all('td')[1].find_all('a')[1].text.strip()
-    xp = tr.find_all('td')[3].text.strip()
-    print(place, username,xp, sep=' ')
+    place.append(tr.find_all('td')[0].text.strip())
+    username.append(tr.find_all('td')[1].find_all('a')[1].text.strip())
+    xp.append(tr.find_all('td')[3].text.strip())
+
+df = pd.DataFrame({'Place':place, 'Username':username, 'XP':xp})
+df.to_csv('output.csv')
+
